50ada3df1a9145f0f7802f4c8da9e2fe1871f1c7
OliverBR1 <Br.0liveira@outlook.com>
2026-09-16 22:32:36 -0300

    add import

--- src/test/java/tech/oliver/orderworkerms/OrderworkermsApplicationTests.java
+++ src/test/java/tech/oliver/orderworkerms/OrderworkermsApplicationTests.java
@@ -3,6 +3,7 @@ package tech.oliver.orderworkerms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.utility.TestcontainersConfiguration;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
