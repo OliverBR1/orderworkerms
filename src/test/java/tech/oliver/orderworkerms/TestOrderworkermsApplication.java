e946290b51dcc4c769b2cce7167587db9eec7ac5
OliverBR1 <Br.0liveira@outlook.com>
2026-09-16 22:33:01 -0300

    add localStackContainer

--- src/test/java/tech/oliver/orderworkerms/TestOrderworkermsApplication.java
+++ src/test/java/tech/oliver/orderworkerms/TestOrderworkermsApplication.java
@@ -2,10 +2,26 @@ package tech.oliver.orderworkerms;
 
 import org.springframework.boot.SpringApplication;
 
+import java.util.List;
+import java.util.Map;
+
+import static tech.oliver.orderworkerms.ContainersConfig.*;
+
 public class TestOrderworkermsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(OrderworkermsApplication::main).with(TestcontainersConfiguration.class).run(args);
+		localStackContainer.setPortBindings(List.of("4566:4566"));
+		localStackContainer.start();
+		setupSqs();
+        for (Map.Entry<String, Object> entry : getProperties().entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            System.setProperty(key, (String) value);
+        }
+
+        SpringApplication.from(OrderworkermsApplication::main)
+				.with(ServiceConnectionConfig.class)
+				.run(args);
 	}
 
 }
